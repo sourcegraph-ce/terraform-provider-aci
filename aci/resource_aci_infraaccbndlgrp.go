547011751b944a068e60b219e632059b78a630e4
Sourcegraph <batch-changes@sourcegraph.com>
2023-03-18 00:31:43 +0000

    Replacing log package with logrus

--- aci/resource_aci_infraaccbndlgrp.go
+++ aci/resource_aci_infraaccbndlgrp.go
@@ -2,7 +2,7 @@ package aci
 
 import (
 	"fmt"
-	"log"
+	log "github.com/sourcegraph-ce/logrus"
 	"reflect"
 	"sort"
 
--- vendor/github.com/ciscoecosystem/aci-go-client/client/cloudCtxProfile_service.go
+++ vendor/github.com/ciscoecosystem/aci-go-client/client/cloudCtxProfile_service.go
@@ -2,7 +2,7 @@ package client
 
 import (
 	"fmt"
-	"log"
+	log "github.com/sourcegraph-ce/logrus"
 
 	"github.com/ciscoecosystem/aci-go-client/container"
 	"github.com/ciscoecosystem/aci-go-client/models"
